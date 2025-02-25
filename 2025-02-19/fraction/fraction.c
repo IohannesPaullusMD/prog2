f6c01210c9968d7318cfef1be77ef6df9e39ba50
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2025-02-25 11:37:03 +0800

    feat: completed the code for fraction.c

--- 2025-02-19/fraction/fraction.c
+++ 2025-02-19/fraction/fraction.c
@@ -2,161 +2,106 @@
 #include "fraction.h"
 #include "../error/error.h"
 
-int euclideanAlgo(int a, int b);
-
-_Fraction createFraction(int numerator, int denominator)
-{
-    if (denominator == 0)
+_Fraction createFraction(int numerator, int denominator) {
+    if (denominator == 0) 
     {
-        throwError("undefined fraction: 0 denominator");
+        throwError("undefined fraction: division by 0");
     }
 
-    return (_Fraction) {0, numerator, denominator};
+    _Fraction fraction;
+    fraction.whole = 0;
+    fraction.num = numerator;
+    fraction.den = denominator;
+    return simplifyFraction(fraction);
 }
 
-void displayFraction(_Fraction fraction)
-{
-    if (fractionForm(fraction) == MIX_FORM)
-    {
-        fraction = makeImproperFraction(fraction);
+void displayFraction(_Fraction fraction) {
+    if (fraction.whole != 0) {
+        printf("%d ", fraction.whole);
     }
-
-    printf("%d / %d\n", fraction.num, fraction.den);
+    if (fraction.num != 0) {
+        printf("%d/%d", fraction.num, fraction.den);
+    }
+    if (fraction.whole == 0 && fraction.num == 0) {
+        printf("0");
+    }
+    printf("\n");
 }
 
-int euclideanAlgo(int a, int b) 
-{
+_Fraction sumFraction(_Fraction f1, _Fraction f2) {
+    _Fraction result;
+    result.num = f1.num * f2.den + f2.num * f1.den;
+    result.den = f1.den * f2.den;
+    result.whole = 0;
+    return simplifyFraction(result);
+}
+
+_Fraction diffFraction(_Fraction f1, _Fraction f2) {
+    f2.num = -f2.num;
+    return sumFraction(f1, f2);
+}
+
+_Fraction prodFraction(_Fraction f1, _Fraction f2) {
+    _Fraction result;
+    result.num = f1.num * f2.num;
+    result.den = f1.den * f2.den;
+    result.whole = 0;
+    return simplifyFraction(result);
+}
+
+_Fraction quoFraction(_Fraction f1, _Fraction f2) {
+    _Fraction result;
+    result.num = f1.num * f2.den;
+    result.den = f1.den * f2.num;
+    result.whole = 0;
+    return simplifyFraction(result);
+}
+
+_Fraction reciprocalFraction(_Fraction fraction) {
+    _Fraction result;
+    result.num = fraction.den;
+    result.den = fraction.num;
+    result.whole = 0;
+    return result;
+}
+
+/**
+ * converts an improper fraction
+ * to a mix fraction.
+ * 3/2 = 1 1/2 
+ */
+_Fraction makeMixFraction(_Fraction fraction) {
+    fraction.whole = fraction.num / fraction.den;
+    fraction.num = fraction.num % fraction.den;
+    return fraction;
+}
+
+_Fraction makeImproperFraction(_Fraction fraction) {
+    fraction.num = fraction.whole * fraction.den + fraction.num;
+    fraction.whole = 0;
+    return fraction;
+}
+
+_Fraction simplifyFraction(_Fraction fraction) {
+    int gcd, a = fraction.num, b = fraction.den;
     while (b != 0) {
         int temp = b;
         b = a % b;
         a = temp;
     }
-    return a;
-}
-
-_Fraction sumFraction(_Fraction f1, _Fraction f2)
-{
-    f1 = makeImproperFraction(f1);
-    f2 = makeImproperFraction(f2);
-    
-    if (f1.num == 0)
-    {
-        return f2;
-    } 
-
-    if (f2.num == 0)
-    {
-        return f1;
-    }
-
-    if (f1.den != f2.den)
-    {
-        f1 = prodFraction
-        (
-            f1,
-            createFraction(0, f2.den, f2.den)
-        );
-        f2 = prodFraction
-        (
-            f2,
-            createFraction(0, f1.den, f1.den)
-        );
-    }
-
-    return createFraction
-    (
-        f1.num + f2.num,
-        f1.den
-    );
-}
-
-_Fraction diffFraction(_Fraction f1, _Fraction f2)
-{
-    f2.num *= -1;
-    return sumFraction(f1, f2);
-}
-
-_Fraction prodFraction(_Fraction f1, _Fraction f2)
-{
-    f1 = makeImproperFraction(f1);
-    f2 = makeImproperFraction(f2);
-
-    return createFraction
-    (
-        (f1.num * f2.num),
-        (f1.den * f2.den)
-    );
-}
-
-_Fraction quoFraction(_Fraction f1, _Fraction f2)
-{
-    if (f2.num == 0 || f2.den == 0)
-    {
-        throwError("error: division by zero");
-    }
-
-    return prodFraction(f1, reciprocalFraction(f2));
-}
-
-_Fraction reciprocalFraction(_Fraction fraction)
-{
-    fraction = makeImproperFraction(fraction);
-    int temp = fraction.num;
-    fraction.num = fraction.den;
-    fraction.den = temp;
+    gcd = (a < 0) ? -a : a;
+    fraction.num /= gcd;
+    fraction.den /= (gcd == 0) ? 1 : gcd;
     return fraction;
 }
 
-_Fraction makeMixFraction(_Fraction fraction)
-{
-    if (fractionForm(fraction) == IMPROPER_FORM)
-    {
-        fraction.whole = fraction.num / fraction.den;
-        fraction.num = fraction.num % fraction.den;
-
-        if (fraction.num == 0)
-        {
-            fraction.num = fraction.den = 1;
-        }
-    }
-
-    return simplifyFraction(fraction);
-}
-
-_Fraction makeImproperFraction(_Fraction fraction)
-{
-    if (fractionForm(fraction) == MIX_FORM)
-    {
-        int n = fraction.whole * fraction.den;
-        fraction.num *= n;
-        fraction.den *= n;
-        fraction.whole = 0;
-    }
-
-    return fraction;
-}
-
-
-uShort fractionForm(_Fraction fraction)
-{
-    if (fraction.whole != 0)
-    {
+uShort fractionForm(_Fraction fraction) {
+    if (fraction.whole != 0) {
         return MIX_FORM;
-    }
-    else if (fraction.num > fraction.den)
-    {
+    } else if (fraction.num < fraction.den) {
+        return PROPER_FORM;
+    } else {
         return IMPROPER_FORM;
     }
-    
-    return PROPER_FORM;
 }
 
-_Fraction simplifyFraction(_Fraction fraction)
-{
-    fraction = makeImproperFraction(fraction);
-    int common = euclideanAlgo(fraction.num, fraction.den);
-    fraction.num /= common;
-    fraction.den /= common;
-
-    return fraction;
-}
--- 2025-02-19/main.c
+++ 2025-02-19/main.c
@@ -1,6 +1,106 @@
 #include <stdio.h>
+#include <assert.h>
+#include "fraction/fraction.h"
 
-int main()
-{
-    printf("%d", 13 % 12);
+int main() {
+    // Test createFraction
+    _Fraction f1 = createFraction(3, 4);
+    _Fraction f2 = createFraction(5, 6);
+    _Fraction f3 = createFraction(7, 2);
+    _Fraction f4 = createFraction(9, 3);
+    _Fraction f5 = createFraction(4, 8);
+    _Fraction f6 = createFraction(10, 20);
+
+    // Test displayFraction
+    printf("Test displayFraction:\n");
+    displayFraction(f1);
+    displayFraction(f2);
+    displayFraction(f3);
+
+    // Test sumFraction
+    printf("\nTest sumFraction:\n");
+    _Fraction sum1 = sumFraction(f1, f2);
+    _Fraction sum2 = sumFraction(f3, f4);
+    _Fraction sum3 = sumFraction(f5, f6);
+    assert(sum1.num == 19 && sum1.den == 12);
+    assert(sum2.num == 13 && sum2.den == 2);
+    assert(sum3.num == 1 && sum3.den == 1);
+
+    // Test diffFraction
+    printf("\nTest diffFraction:\n");
+    _Fraction diff1 = diffFraction(f1, f2);
+    _Fraction diff2 = diffFraction(f3, f4);
+    _Fraction diff3 = diffFraction(f5, f6);
+    assert(diff1.num == -1 && diff1.den == 12);
+    assert(diff2.num == 1 && diff2.den == 2);
+    assert(diff3.num == 0 && diff3.den == 1);
+
+    // Test prodFraction
+    printf("\nTest prodFraction:\n");
+    _Fraction prod1 = prodFraction(f1, f2);
+    _Fraction prod2 = prodFraction(f3, f4);
+    _Fraction prod3 = prodFraction(f5, f6);
+    assert(prod1.num == 5 && prod1.den == 8);
+    assert(prod2.num == 21 && prod2.den == 2);
+    assert(prod3.num == 1 && prod3.den == 4);
+
+    // Test quoFraction
+    printf("\nTest quoFraction:\n");
+    _Fraction quo1 = quoFraction(f1, f2);
+    _Fraction quo2 = quoFraction(f3, f4);
+    _Fraction quo3 = quoFraction(f5, f6);
+    assert(quo1.num == 9 && quo1.den == 10);
+    assert(quo2.num == 7 && quo2.den == 6);
+    assert(quo3.num == 1 && quo3.den == 1);
+
+    // Test reciprocalFraction
+    printf("\nTest reciprocalFraction:\n");
+    _Fraction rec1 = reciprocalFraction(f1);
+    _Fraction rec2 = reciprocalFraction(f3);
+    _Fraction rec3 = reciprocalFraction(f5);
+    assert(rec1.num == 4 && rec1.den == 3);
+    assert(rec2.num == 2 && rec2.den == 7);
+    assert(rec3.num == 2 && rec3.den == 1);
+
+    // Test makeMixFraction
+    printf("\nTest makeMixFraction:\n");
+    _Fraction mix1 = makeMixFraction(f3);
+    _Fraction mix2 = makeMixFraction(f4);
+    _Fraction mix3 = makeMixFraction(f6);
+    assert(mix1.whole == 3 && mix1.num == 1 && mix1.den == 2);
+    assert(mix2.whole == 3 && mix2.num == 0 && mix2.den == 1);
+
+    // Test makeImproperFraction
+    printf("\nTest makeImproperFraction:\n");
+    _Fraction improper1 = makeImproperFraction(mix1);
+    _Fraction improper2 = makeImproperFraction(mix2);
+    _Fraction improper3 = makeImproperFraction(mix3);
+    assert(improper1.num == 7 && improper1.den == 2);
+    assert(improper2.num == 3 && improper2.den == 1);
+    assert(improper3.num == 1 && improper3.den == 2);
+
+    // Test simplifyFraction
+    printf("\nTest simplifyFraction:\n");
+    _Fraction unsimplified1 = createFraction(8, 12);
+    _Fraction unsimplified2 = createFraction(18, 24);
+    _Fraction unsimplified3 = createFraction(50, 100);
+    _Fraction simplified1 = simplifyFraction(unsimplified1);
+    _Fraction simplified2 = simplifyFraction(unsimplified2);
+    _Fraction simplified3 = simplifyFraction(unsimplified3);
+    assert(simplified1.num == 2 && simplified1.den == 3);
+    assert(simplified2.num == 3 && simplified2.den == 4);
+    assert(simplified3.num == 1 && simplified3.den == 2);
+
+    // Test fractionForm
+    printf("\nTest fractionForm:\n");
+    uShort form1 = fractionForm(f1);
+    uShort form2 = fractionForm(f3);
+    uShort form3 = fractionForm(mix1);
+    assert(form1 == PROPER_FORM);
+    assert(form2 == IMPROPER_FORM);
+    assert(form3 == MIX_FORM);
+
+    printf("All tests passed!\n");
+
+    return 0;
 }
