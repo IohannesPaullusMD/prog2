692a88e6251402d92352ea607e92bc31b202ccdd
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2025-03-17 12:27:58 +0800

    ASSIGNMENT AHH...

--- 2023-03-17-assignment/main.c
+++ 2023-03-17-assignment/main.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
-#include "my_header.h"
 #include <stdlib.h>
+
+#include "my_header.h"
 
 #define EXIT 0
 #define DISPLAY_LIST 1
-#define INSERT_SORTED 2
-#define GET_PRIMES 3
+#define ADD_ELEMENT 2
+#define REMOVE_ELEMENT 3
+#define IS_EMPTY 4
+#define IS_FULL 5
+#define GET_ALL_PRIME_FACTORS 6
 
 void displayCommands();
-bool executeCommand(int command, List *list);
-void handleInsertCommand(List *list);
-void handleGetPrimesCommand(List *list);
+bool executeCommand(int command, ArrayList *list);
+void addElementHandler(ArrayList *list);
+void isEmptyHandler(ArrayList list);
+void isFullHandler(ArrayList list);
+void removeElementHandler(ArrayList *list);
+void getAllPrimeFactorsHandler(ArrayList *list);
+
 
 int main()
 {
-    int command = 0;
-    List list = createList();
-    initList(&list);
+    int command = EXIT;
+    ArrayList list = createList(1);
 
     do
     {
@@ -32,16 +39,19 @@ void displayCommands()
     system("cls");
     printf
     (
-        "%s%s%s%s%s",
+        "%s%s%s%s%s%s%s%s",
         "Enter the ID of the command you want to execute.\n",
         "[0] exit\n",
         "[1] display list\n",
-        "[2] insert sorted\n",
-        "[3] get prime numbers in list\n"
+        "[2] add element\n",
+        "[3] remove element\n",
+        "[4] isEmpty\n",
+        "[5] isFull\n",
+        "[6] remove all prime factors in list and display removed items\n"
     );
 }
 
-bool executeCommand(int command, List *list)
+bool executeCommand(int command, ArrayList *list)
 {
     printf("\n");
     switch (command)
@@ -51,11 +61,20 @@ bool executeCommand(int command, List *list)
         case DISPLAY_LIST:
             displayList(*list);
             break;
-        case INSERT_SORTED:
-            handleInsertCommand(list);
+        case ADD_ELEMENT:
+            addElementHandler(list);
             break;
-        case GET_PRIMES:
-            handleGetPrimesCommand(list);
+        case REMOVE_ELEMENT:
+            removeElementHandler(list);
+            break;
+        case IS_EMPTY:
+            isEmptyHandler(*list);
+            break;
+        case IS_FULL:
+            isFullHandler(*list);
+            break;
+        case GET_ALL_PRIME_FACTORS:
+            getAllPrimeFactorsHandler(list);
             break;
     }
 
@@ -64,23 +83,49 @@ bool executeCommand(int command, List *list)
     return true;
 }
 
-void handleInsertCommand(List *list)
+void addElementHandler(ArrayList *list)
 {
-    int item = 0;
-    printf("Enter element to insert: ");
-    scanf("%d", &item);
+    int element = 0;
+    printf("Enter element to add: ");
+    scanf("%d", &element);
     printf
     (
-        "%s inserted: %d \n",
-        insertSorted(list, item) ? "successfully" : "not successfully",
-        item
+        "%s added element: %d\n",
+        addElement(list, element) ? "Successfully" : "Not Successfully",
+        element
     );
 }
 
-void handleGetPrimesCommand(List *list)
+void isEmptyHandler(ArrayList list)
 {
-    printf("removed items:\n");
-    displayList(getPrimeNumbers(list));
-
-    displayList(*list);
+    printf("list is %s\n", isEmpty(list) ? "empty" : "not empty");
 }
+
+void isFullHandler(ArrayList list)
+{
+    printf("list is %s\n", isFull(list) ? "full" : "not full");
+}
+
+void removeElementHandler(ArrayList *list)
+{
+    int element = 0;
+    printf("Enter element to remove: ");
+    scanf("%d", &element);
+    printf
+    (
+        "%s removed element: %d\n",
+        addElement(list, element) ? "Successfully" : "Not Successfully",
+        element
+    );
+}
+
+void getAllPrimeFactorsHandler(ArrayList *list)
+{
+    int number = 0;
+    printf("Enter number to find its prime factors in the list: ");
+    scanf("%d", &number);
+
+    printf("\nPrime factors of %d removed from the list:\n", number);
+    displayList(getAllPrimeFactors(list, number));
+}
+
--- 2023-03-17-assignment/my_header.c
+++ 2023-03-17-assignment/my_header.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "my_header.h"
 
 bool isPrime(int num);
+bool doubleCapacity(ArrayList *list);
+bool isPrimeFactor(int test, int num);
 
 void initList(ArrayList *list, uInt capacity) 
 {
@@ -10,7 +14,7 @@ void initList(ArrayList *list, uInt capacity)
     list->size = 0;
     if (list->arr != NULL)
     {
-        list->capacity = capacity;
+        list->capacity = (capacity < 1 ? 1 : capacity);
     }
 }
 
@@ -44,44 +48,77 @@ void displayList(const ArrayList list)
  */
 bool addElement(ArrayList *list, int item)
 {
-    if (list->size == list->capacity) 
+    if (isFull(*list) && !doubleCapacity(list)) 
     {
-        int *temp = realloc
-        (
-            list->arr, 
-            (list->capacity << 1) * sizeof(int)
-        );
-
-        if (temp == NULL) 
-        {
-            return false;
-        }
-
-        list->capacity = list->capacity << 1;
+        return false;
     }
-    
+
+    memcpy(list + 1, list, list->size * sizeof(int));
+    list->arr[0] = item;
+    ++list->size;
+    return true;
 }
 
-// TODO: remove primes in list then return the removed items
-List getPrimeNumbers(List *list)
+// {} -> 0
+// {1} -> 1
+// {1,2} -> 1
+// {1,2,3} -> 2
+bool removeElement(ArrayList *list, int item)
 {
-    List notPrimes = createList();
-    int newCount = 0;
+    bool isItemFound = false;
+    int i;
+    for (i = 0; i < list->size && list->arr[i] != item; ++i);
 
-    for (int i = 0; i < list->count; ++i)
+    if (i < list->size && list->arr[i] == item)
     {
-        if (isPrime(list->arr[i]))
+        isItemFound = true;
+        for (++i; i < list->size; ++i)
         {
-            list->arr[newCount++] = list->arr[i];
+            list->arr[i-1] = list->arr[i];
+        }
+    }
+    
+    return isItemFound;
+}
+
+bool isEmpty(const ArrayList list)
+{
+    return list.size == 0;
+}
+
+bool isFull(const ArrayList list)
+{
+    return list.size == list.capacity;
+}
+
+/**
+ * remove all prime factors of value in list.
+ * return all removed values.
+ */
+ArrayList getAllPrimeFactors(ArrayList *list, int value)
+{
+    ArrayList primeFactors = createList(list->size);
+    int newSize = 0;
+
+    for (int i = 0; i < list->size; ++i)
+    {
+        if (isPrimeFactor(list->arr[i], value))
+        {
+            primeFactors.arr[primeFactors.size++] = list->arr[i];
         }
         else
         {
-            notPrimes.arr[notPrimes.count++] = list->arr[i];
+            list->arr[newSize++] = list->arr[i];
         }
     }
 
-    list->count = newCount;
-    return notPrimes;
+    list->size = newSize;
+    return primeFactors;
+}
+
+bool isPrimeFactor(int test, int num)
+{
+    return isPrime(test) && (num % test) == 0;
 }
 
 bool isPrime(int num) {
@@ -96,4 +133,22 @@ bool isPrime(int num) {
     }
     
     return true; 
+}
+
+bool doubleCapacity(ArrayList *list)
+{
+    int *temp = realloc
+    (
+        list->arr, 
+        (list->capacity << 1) * sizeof(int)
+    );
+
+    if (temp == NULL) 
+    {
+        return false;
+    }
+
+    list->arr = temp;
+    list->capacity = list->capacity << 1;
+    return true;
 }
